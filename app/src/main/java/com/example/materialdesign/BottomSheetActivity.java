7ab8971bc1a6ba8e7afc93f900d9afefa4e5e5ae
mr.Blue <yashinsergey125@gmail.com>
2019-11-29 16:49:03 +0300

    bottom sheet added

---
+++ app/src/main/java/com/example/materialdesign/BottomSheetActivity.java
@@ -0,0 +1,15 @@
+package com.example.materialdesign;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
