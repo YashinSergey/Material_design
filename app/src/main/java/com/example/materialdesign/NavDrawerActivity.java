fc897e2ab64ea1dc92142cea6dfe8d920135ee7a
mr.Blue <yashinsergey125@gmail.com>
2019-12-05 14:34:39 +0300

    Added NavigationItemSelectedListener

--- app/src/main/java/com/example/materialdesign/NavDrawerActivity.java
+++ app/src/main/java/com/example/materialdesign/NavDrawerActivity.java
@@ -2,19 +2,24 @@ package com.example.materialdesign;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class NavDrawerActivity extends AppCompatActivity{
+public class NavDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
 
     @Override
@@ -28,12 +33,42 @@ public class NavDrawerActivity extends AppCompatActivity{
         toggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (toggle.onOptionsItemSelected(item)) return true;
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.dashboard:
+                Toast.makeText(getApplicationContext(), "Dashboard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.event:
+                Toast.makeText(getApplicationContext(), "Event", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "Search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.activities:
+                Toast.makeText(getApplicationContext(), "Activities", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                finish();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
