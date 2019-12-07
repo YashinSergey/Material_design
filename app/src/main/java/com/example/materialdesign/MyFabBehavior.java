62a38540958e21e364ca9d34b80e07a249da6d4b
mr.Blue <yashinsergey125@gmail.com>
2019-12-07 22:14:19 +0300

    added MyFabBehavior

---
+++ app/src/main/java/com/example/materialdesign/MyFabBehavior.java
@@ -0,0 +1,45 @@
+package com.example.materialdesign;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MyFabBehavior extends FloatingActionButton.Behavior {
+
+    public MyFabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child,
+                               @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed,
+                               int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
+                dxUnconsumed, dyUnconsumed, type);
+        if (dyConsumed > 0) {
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            int fabBottomMargin = layoutParams.bottomMargin;
+            child.animate().translationY(child.getHeight() + fabBottomMargin)
+                    .setInterpolator(new LinearInterpolator()).start();
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull FloatingActionButton child, @NonNull View directTargetChild,
+                                       @NonNull View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+}
